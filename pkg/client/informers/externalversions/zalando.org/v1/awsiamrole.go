b13eaddea338fb5d8ebce2927aa45db652d08c3d
Carsten Giersberg <carsten.giersberg@zalando.de>
2026-03-17 10:53:06 +0100

    add other changes

--- pkg/client/informers/externalversions/zalando.org/v1/awsiamrole.go
+++ pkg/client/informers/externalversions/zalando.org/v1/awsiamrole.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2025 The Kubernetes Authors.
+Copyright 2026 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -57,20 +57,32 @@ func NewAWSIAMRoleInformer(client versioned.Interface, namespace string, resyncP
 // one. This reduces memory footprint and number of connections to the server.
 func NewFilteredAWSIAMRoleInformer(client versioned.Interface, namespace string, resyncPeriod time.Duration, indexers cache.Indexers, tweakListOptions internalinterfaces.TweakListOptionsFunc) cache.SharedIndexInformer {
 	return cache.NewSharedIndexInformer(
-		&cache.ListWatch{
+		cache.ToListWatcherWithWatchListSemantics(&cache.ListWatch{
 			ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.ZalandoV1().AWSIAMRoles(namespace).List(context.TODO(), options)
+				return client.ZalandoV1().AWSIAMRoles(namespace).List(context.Background(), options)
 			},
 			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.ZalandoV1().AWSIAMRoles(namespace).Watch(context.TODO(), options)
+				return client.ZalandoV1().AWSIAMRoles(namespace).Watch(context.Background(), options)
 			},
-		},
+			ListWithContextFunc: func(ctx context.Context, options metav1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.ZalandoV1().AWSIAMRoles(namespace).List(ctx, options)
+			},
+			WatchFuncWithContext: func(ctx context.Context, options metav1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.ZalandoV1().AWSIAMRoles(namespace).Watch(ctx, options)
+			},
+		}, client),
 		&apiszalandoorgv1.AWSIAMRole{},
 		resyncPeriod,
 		indexers,
